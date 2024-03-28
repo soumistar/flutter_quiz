a4dbf6eb89d073d7cc459936e04d34609612b4f0
soumistar <ismailatoure435@gmail.com>
2024-03-28 15:38:12 +0000

    Naviguer vers la seconde page

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/widgets/page_quiz.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -12,50 +14,52 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 10,
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title,
-      style: const TextStyle(
-        color: Colors.white,
-      ),
-      ),
-      centerTitle: true,
-      
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Card(
-            elevation: 10,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 2.5,
-              width: MediaQuery.of(context).size.width / 1.5,
-              child: Image.asset("assets/camavinga.jpg", 
-              fit: BoxFit.cover,
-              ),
-
-            ),
-          ),
-          const SizedBox(height: 90,),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue
-            ),
-            onPressed: (){
-
-          }, child: const Text("Commencer le quizz",
-          textScaleFactor: 2,
-          style: TextStyle(
+      appBar: AppBar(
+        elevation: 10,
+        backgroundColor: Colors.blue,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
             color: Colors.white,
-            fontStyle: FontStyle.italic
           ),
-          )
-          )
-        ],
+        ),
+        centerTitle: true,
       ),
-    ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              elevation: 10,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2.5,
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: Image.asset(
+                  "assets/camavinga.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 90,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                    return PageQuizz();
+                  }));
+                },
+                child: const Text(
+                  "Commencer le quizz",
+                  textScaleFactor: 2,
+                  style: TextStyle(
+                      color: Colors.white, fontStyle: FontStyle.italic),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
