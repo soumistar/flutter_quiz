bb466bb5fb71d4dde7989094a998f10f632983b7
soumistar <ismailatoure435@gmail.com>
2024-03-28 15:15:45 +0000

    UI de la première page et CustomText

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -13,8 +13,49 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          elevation: 10,
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-    ));
+      title: Text(widget.title,
+      style: const TextStyle(
+        color: Colors.white,
+      ),
+      ),
+      centerTitle: true,
+      
+    ),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Card(
+            elevation: 10,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: Image.asset("assets/camavinga.jpg", 
+              fit: BoxFit.cover,
+              ),
+
+            ),
+          ),
+          const SizedBox(height: 90,),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue
+            ),
+            onPressed: (){
+
+          }, child: const Text("Commencer le quizz",
+          textScaleFactor: 2,
+          style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic
+          ),
+          )
+          )
+        ],
+      ),
+    ),
+    );
   }
 }
